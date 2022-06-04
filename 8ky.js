cb86fe576838045128eaa716c94724cc9546d639
Faith <fhgray96@gmail.com>
2022-06-04 17:55:08 -0500

    solved problem

---
+++ 8ky.js
@@ -0,0 +1,7 @@
+//Convert a Boolean to a string
+//Implement a function which convert the given boolean value into its string representation
+function booleanToString(b){
+    //your code here
+    let message = b === true ? "true" : "false";
+    return message;
+  }

1229421598b86c453cf38a9309e9d98fa14f47d0
Faith <fhgray96@gmail.com>
2022-06-21 21:05:31 -0500

    add solution

--- 6ky/WhichAreIn.js
+++ 6ky/WhichAreIn.js
@@ -8,3 +8,21 @@
 //     Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
 //     In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
 //     Beware: r must be without duplicates.
+
+Array;function inArray(array1,array2){
+    //...
+    let newArray = [];
+    array1.sort();
+    
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array2[j].indexOf(array1[i]) !== -1) {
+          newArray.push(array1[i]);
+          break;
+        }
+      }
+    } 
+    return newArray;
+}
+
+//This is my first time using a nested array. Super cool

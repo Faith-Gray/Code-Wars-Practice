ec17615f0df932d236dc8b954ea0786bddcb2413
Faith <fhgray96@gmail.com>
2022-06-25 11:12:32 -0500

    add problem

---
+++ 6ky/ConvertStringToCamelCase.js
@@ -0,0 +1,8 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+// Examples
+
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+
+//I don't quite understand this one yet. 6kyu problems are possibly still a little bit harder than what I'm used to. 

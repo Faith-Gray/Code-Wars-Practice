d1458ce4b03c2b0b1e1504aa8683089e9a4d9197
Faith Gray <89816245+Faith-Gray@users.noreply.github.com>
2022-11-27 12:14:35 -0600

    Update NarcissticNumbers.js

--- NarcissticNumbers.js
+++ NarcissticNumbers.js
@@ -13,3 +13,35 @@
 // Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
 
 // Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+function narcissistic(value) {
+function narcissistic(value) {
+  // Code me to return true or false
+  let x = 0;
+  let new = 0;
+  if (value > 9) {
+    x = value.toString().split('')
+    for (let i = 0; i < x.length; i++){
+      new += Math.pow(Number(x[i]), x.length)
+    }
+    return new ===value ? true:false
+  } else {
+    return true
+  }
+}
+
+  
+//   function narcissistic(value) {
+//   // Code me to return true or false
+//   let newArr = [];
+//   let result = 0;
+  
+//   let number = value.length();
+  
+//   for (let i = 0; i < number; i++) {
+//   newArry.push(math.pow(value[i], number))
+//   } 
+//     for (let i = 0; i < newArr.length; i++) {
+//  result = newArr[i] + result;   } 
+//   return result;
+// }
